22742233fb485a19e48a42e828de1a16caf540b3
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2021-10-12 09:32:28 +0200

    init

---
+++ src/main/kotlin/dev/aleixmorgadas/pocactivemqhttp/PocactivemqhttpApplication.kt
@@ -0,0 +1,11 @@
+package dev.aleixmorgadas.pocactivemqhttp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocactivemqhttpApplication
+
+fun main(args: Array<String>) {
+	runApplication<PocactivemqhttpApplication>(*args)
+}
---
+++ src/test/kotlin/dev/aleixmorgadas/pocactivemqhttp/PocactivemqhttpApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.aleixmorgadas.pocactivemqhttp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocactivemqhttpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
